33a5cdc1c2c3813db7543f25ca31efc34e415789
harleen-03 <rijakbhatia03@gmail.com>
2026-06-26 02:54:23 +0530

    Creating user entity

---
+++ src/main/java/com/tripbharat/tripbharat/entity/User.java
@@ -0,0 +1,36 @@
+package com.tripbharat.tripbharat.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+}
